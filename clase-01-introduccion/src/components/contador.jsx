8c7c806618ffd82a385607863aef20ab17638714
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-11-04 15:50:32 -0300

    feat(components): agregar componente Contador

---
+++ clase-01-introduccion/src/components/contador.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+const Contador = () => {
+
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => setContador(contador + 1);
+  const decrementar = () => setContador(contador - 1);
+  const resetear = () => setContador(0);
+
+  return (
+
+    <div>
+      <h2>Contador: {contador}</h2>
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
+      <button onClick={resetear}>Resetear</button>
+    </div>
+  );
+
+};
+
+export default Contador;
